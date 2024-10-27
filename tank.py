105ce72a559c2b6773ed41635ac07303c86d2321
Boss <kosevaa@mail.ru>
2024-10-27 13:17:47 +0300

    qfg

--- tank.py
+++ tank.py
@@ -1,10 +1,12 @@
 import random
 import time
 import wrap
-a=1
+rr=random.randint(50,550)
+timepulfly=time.time()
+timebonus=time.time()
 wrap.world.create_world(600, 600)
 tank1=wrap.sprite.add("battle_city_tanks",200,100,"tank_enemy_size1_green1")
-pul = wrap.sprite.add("battle_city_items", -30, 300, "bullet")
+pul = wrap.sprite.add("battle_city_items", -30, rr, "bullet")
 wrap.sprite.set_angle(pul,90)
 wrap.sprite.set_size(pul,50,50)
 @wrap.on_key_always(wrap.K_LEFT,wrap.K_RIGHT)
@@ -21,11 +23,19 @@ def move(keys):
 
 @wrap.always()
 def pulfly():
-    wrap.sprite.move_at_angle_point(pul,700,300,50)
+    global timepulfly,rr
+    wrap.sprite.move_at_angle_point(pul,700,rr,25)
     x=wrap.sprite.get_x(pul)
     print(x)
-    if x>=700:
-        wrap.sprite.move_to(pul,-30,300)
-    print(time.time())
 
+    if x>=700 and time.time() - timepulfly >= 5:
+        timepulfly=time.time()
+        rr=random.randint(50,550)
+        wrap.sprite.move_to(pul,-30,rr)
+@wrap.always()
+def bonus():
+    global timebonus
+    if time.time() - timebonus >= 2:
+        wrap.sprite.add("battle_city_items",random.randint(50,550),random.randint(50,550),"block_gift_star")
+        timebonus=time.time()
 
